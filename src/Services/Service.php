70ef30fcf4c4bb7a85d15b399b93a355864af407
Jordan Dobrev <razpusni.bg@gmail.com>
2015-07-21 15:13:00 +0100

    fixes for keyBy grouping for array response

--- src/Services/Service.php
+++ src/Services/Service.php
@@ -5,24 +5,44 @@ use Buzz\Control\Contracts\Client;
 use Buzz\Control\Filter;
 use Buzz\Control\GuzzleClient;
 
+/**
+ * Class Service
+ *
+ * @package Buzz\Control\Services
+ */
 abstract class Service
 {
     /**
-     * @var
+     * @var string
      */
     protected static $cast;
+
+    /**
+     * @var string
+     */
+    protected $keyBy = null;
 
     /**
      * @var Filter
      */
     protected $filter;
 
+    /**
+     * @param Client $client
+     */
     public final function __construct(Client $client = null)
     {
         $this->client = $client ?: new GuzzleClient(Buzz::getCredentials());
     }
 
-    public function where($parameter, $operator, $value)
+    /**
+     * @param $parameter
+     * @param $operator
+     * @param $value
+     *
+     * @return Service
+     */
+    public final function where($parameter, $operator, $value)
     {
         $clone = clone $this;
 
@@ -37,11 +57,29 @@ abstract class Service
         return $clone;
     }
 
+    /**
+     * @param string $keyBy
+     */
+    public final function keyBy($keyBy = 'id')
+    {
+        $this->keyBy = $keyBy;
+    }
+
+    /**
+     * @param ...$parameters
+     *
+     * @return mixed
+     */
     protected final function callAndCast(...$parameters)
     {
         return $this->cast($this->call(...$parameters));
     }
 
+    /**
+     * @param $response
+     *
+     * @return mixed
+     */
     protected final function cast($response)
     {
         $object = static::$cast;
@@ -49,6 +87,17 @@ abstract class Service
         return new $object($response);
     }
 
+    /**
+     * @param       $verb
+     * @param       $method
+     * @param array $request
+     *
+     * @return mixed
+     * @throws \Buzz\Control\Exceptions\ErrorException
+     * @throws \Buzz\Control\Exceptions\ResponseException
+     * @throws \Buzz\Control\Exceptions\ServerException
+     * @throws \Buzz\Control\Exceptions\UnauthorizedException
+     */
     protected final function call($verb, $method, array $request = [])
     {
         if ($this->filter) {
@@ -58,17 +107,33 @@ abstract class Service
         return $this->client->request($verb, $method, $request);
     }
 
+    /**
+     * @param ...$parameters
+     *
+     * @return mixed
+     */
     protected final function callAndCastMany(...$parameters)
     {
         return $this->castMany($this->call(...$parameters));
     }
 
+    /**
+     * @param $response
+     *
+     * @return mixed
+     */
     protected final function castMany($response)
     {
+        $result = [];
+
         foreach ($response as $key => $value) {
-            $response[$key] = self::cast($value);
+            if (!$this->keyBy) {
+                $result[$key] = self::cast($value);
+            } else {
+                $result[$this->keyBy] = self::cast($value);
+            }
         }
 
-        return $response;
+        return $result;
     }
 }
