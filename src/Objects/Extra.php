ab4469b728f43a87036983b27ad8c4cef34c065b
Jordan Dobrev <jordan.dobrev@livebuzz.co.uk>
2016-08-17 16:47:39 +0100

    add getters and setters for extras

--- src/Objects/Extra.php
+++ src/Objects/Extra.php
@@ -20,4 +20,36 @@ class Extra extends Object
      * @var string
      */
     protected $model_id;
+
+    /**
+     * @return string
+     */
+    public function getModelId()
+    {
+        return $this->model_id;
+    }
+
+    /**
+     * @param string $model_id
+     */
+    public function setModelId($model_id)
+    {
+        $this->model_id = $model_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModelName()
+    {
+        return $this->model_name;
+    }
+
+    /**
+     * @param string $model_name
+     */
+    public function setModelName($model_name)
+    {
+        $this->model_name = $model_name;
+    }
 }
