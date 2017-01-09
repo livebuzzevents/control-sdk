f9aa063677a0537705c49d6830f19946636c2be1
goodevil_git <rolandas.valantinas@livebuzz.co.uk>
2017-01-09 16:05:52 +0000

    add sms message service and template

---
+++ src/Objects/SmsMessageTemplate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Buzz\Control\Objects;
+
+use Buzz\Control\Objects\Traits\HasIdentifier;
+
+/**
+ * Class SmsMessageTemplate
+ *
+ * @package Buzz\Control\Objects\SmsMessageTemplate
+ */
+class SmsMessageTemplate extends Base
+{
+    use HasIdentifier;
+}
