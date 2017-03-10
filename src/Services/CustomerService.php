946caf1f8832b7805a3bfd10f1bbefd571dc739e
Rob Lambell <rob@lambell.info>
2017-03-10 08:54:26 +0000

    e-badge as base64 in json

--- src/Services/CustomerService.php
+++ src/Services/CustomerService.php
@@ -382,8 +382,6 @@ class CustomerService extends Service
             throw new ErrorException('Customer id required!');
         }
 
-        $buffer = $this->call('get', "customer/{$customer->getId()}/e-badge");
-
         header('Content-Type: application/pdf');
         header('Content-disposition: inline; filename="badge.pdf"');
         header('Cache-Control: public, must-revalidate, max-age=0');
@@ -391,7 +389,7 @@ class CustomerService extends Service
         header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
-        echo $buffer;
+        echo base64_decode($this->call('get', "customer/{$customer->getId()}/e-badge")['e-badge']);
 
         return '';
     }
