7ef18b634f743e6bfd8a9b6acf3aa2beed671812
Jordan Dobrev <jordan.dobrev@livebuzz.co.uk>
2017-10-04 14:58:11 +0100

    add prepaid codes

---
+++ src/Campaign/PrepaidCode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Buzz\Control\Campaign;
+
+/**
+ * Class PrepaidCode
+ *
+ * @property string $code
+ * @property string $group
+ * @property int $amount
+ * @property string $charge_id
+ * @property string $payment_provider_id
+ * @property-read \Buzz\Control\Campaign\Charge $charge
+ * @property-read \Buzz\Control\Campaign\PaymentProvider $payment_provider
+ */
+class PrepaidCode extends Object
+{
+}
