00df6d0a460a47b7df452307568c03dbc025f9d8
Jordan Dobrev <jordan.dobrev@livebuzz.co.uk>
2017-12-07 11:37:48 +0000

    add hook

---
+++ src/Campaign/Hook.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Buzz\Control\Campaign;
+
+/**
+ * Class Stream
+ *
+ * @property string $identifier
+ * @property string $name
+ * @property string $event
+ * @property array $filters
+ * @property string $action
+ * @property array $parameters
+ * @property int $priority
+ */
+class Hook extends Object
+{
+}
