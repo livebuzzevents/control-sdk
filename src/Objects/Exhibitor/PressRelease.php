837cfb1824e497ec69654d58bf87520cd7d2a6e1
Rob Lambell <rob@lambell.info>
2018-03-28 11:18:39 +0100

    press release files

--- src/Objects/Exhibitor/PressRelease.php
+++ src/Objects/Exhibitor/PressRelease.php
@@ -1,5 +1,6 @@
 <?php namespace Buzz\Control\Objects\Exhibitor;
 
+use Buzz\Control\Collection;
 use Buzz\Control\Objects\Base;
 use Buzz\Control\Objects\Traits\BelongsToExhibitor;
 use Buzz\Control\Objects\Traits\HasIdentifier;
@@ -22,6 +23,11 @@ class PressRelease extends Base
      * @var string
      */
     protected $content;
+
+    /**
+     * @var \Buzz\Control\Objects\File[]
+     */
+    protected $files;
 
     /**
      * @return string
@@ -53,5 +59,21 @@ class PressRelease extends Base
     public function setContent($content)
     {
         $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFiles()
+    {
+        return $this->files;
+    }
+
+    /**
+     * @param \Buzz\Control\Objects\File[]|\Buzz\Control\Collection $files
+     */
+    public function setFiles($files)
+    {
+        $this->files = new Collection($files);
     }
 }
