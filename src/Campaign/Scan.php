805dacc205af84c8621a0ec7b779a47433eef816
Jacketh <jacketh@live.co.uk>
2022-11-11 09:21:00 +0000

    Drop content capture

--- src/Campaign/Scan.php
+++ src/Campaign/Scan.php
@@ -6,9 +6,6 @@ use Buzz\Control\Campaign\Traits\WithAnswerHelpers;
 use Buzz\Control\Campaign\Traits\WithPropertyHelpers;
 use Buzz\Control\Traits\SupportRead;
 use Buzz\Control\Traits\SupportWrite;
-use Buzz\EssentialsSdk\Cast;
-use Buzz\EssentialsSdk\Collection;
-use Buzz\EssentialsSdk\Exceptions\ErrorException;
 
 /**
  * Class Scan
@@ -30,26 +27,6 @@ class Scan extends SdkObject
         SupportWrite,
         WithAnswerHelpers,
         WithPropertyHelpers;
-
-    /**
-     * @param Customer $customer
-     * @param string $type
-     * @param int $page
-     * @param int $per_page
-     * @return Collection
-     * @throws ErrorException
-     */
-    public function contentCapture(
-        Customer $customer,
-        string $type,
-        $page = 1,
-        $per_page = 50
-    ): Collection {
-        return Cast::many(
-            $this,
-            $this->api()->get($this->getEndpoint("content-capture/{$customer->id}/$type"), compact('page', 'per_page'))
-        );
-    }
 
     /**
      * @param Exhibitor $exhibitor
