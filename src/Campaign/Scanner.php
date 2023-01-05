a7f572507e09e1498ac00c4d141d4b894c36aeb0
Jacketh <jacketh@live.co.uk>
2023-01-05 15:23:57 +0000

    Added signed product leads attribute to scanner model

--- src/Campaign/Scanner.php
+++ src/Campaign/Scanner.php
@@ -24,6 +24,7 @@ use Buzz\Control\Traits\SupportWrite;
  * @property array $details
  * @property-read boolean $handles_crossovers
  * @property-read string $signed_leads_download_link
+ * @property-read string $signed_product_leads_download_link
  * @property-read \Buzz\Control\Campaign\Customer $customer
  * @property-read \Buzz\Control\Campaign\Exhibitor $exhibitor
  * @property-read \Buzz\Control\Campaign\Seminar $seminar
