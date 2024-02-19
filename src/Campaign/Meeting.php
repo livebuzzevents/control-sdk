36376aa8210ba72f5eb5464d7722cd6b063e0b02
Jacketh <jacketh@live.co.uk>
2024-02-19 13:26:14 +0000

    added request meeting endpoint

--- src/Campaign/Meeting.php
+++ src/Campaign/Meeting.php
@@ -6,9 +6,12 @@ namespace Buzz\Control\Campaign;
  * Class Meeting
  *
  * @property string $status
+ * @property string $location_id
+ * @property string $location_other
  * @property string $guest_id
  * @property string $host_id
  * @property string $meeting_slot_id
+ * @property-read \Buzz\Control\Campaign\Theater $location
  * @property-read \Buzz\Control\Campaign\Customer $guest
  * @property-read \Buzz\Control\Campaign\Customer $host
  * @property-read \Buzz\Control\Campaign\MeetingSlot $meeting_slot
--- src/Campaign/MeetingRequest.php
+++ src/Campaign/MeetingRequest.php
@@ -2,17 +2,30 @@
 
 namespace Buzz\Control\Campaign;
 
+use Buzz\EssentialsSdk\Cast;
+use Buzz\EssentialsSdk\SdkObject as EssentialsSdkObject;
+
 /**
- * Class Meeting
+ * Class MeetingRequest
  *
  * @property int $status
+ * @property string $location_id
+ * @property string $location_other
  * @property string $guest_id
  * @property string $host_id
  * @property string $meeting_slot_id
+ * @property-read \Buzz\Control\Campaign\Theater $location
  * @property-read \Buzz\Control\Campaign\Customer $guest
  * @property-read \Buzz\Control\Campaign\Customer $host
  * @property-read \Buzz\Control\Campaign\MeetingSlot $meeting_slot
  */
 class MeetingRequest extends SdkObject
 {
+    public function request(): EssentialsSdkObject
+    {
+        return Cast::single(
+            (new MeetingRequest()),
+            $this->api()->post($this->getEndpoint($this->id . '/request'), request()->all())
+        );
+    }
 }
