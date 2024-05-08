26c7bba5b83f0e3c1f40bb47799e269093a117c0
Jacketh <jacketh@live.co.uk>
2024-05-08 16:16:13 +0100

    added meeting and meeting request methods

--- src/Campaign/Meeting.php
+++ src/Campaign/Meeting.php
@@ -2,17 +2,56 @@
 
 namespace Buzz\Control\Campaign;
 
+use Buzz\Control\Traits\SupportRead;
+use Buzz\EssentialsSdk\Cast;
+use Buzz\EssentialsSdk\SdkObject as EssentialsSdkObject;
+use Illuminate\Support\Collection;
+
 /**
  * Class Meeting
  *
  * @property string $status
+ * @property string $location_id
+ * @property string $location_other
  * @property string $guest_id
  * @property string $host_id
  * @property string $meeting_slot_id
+ * @property-read \Buzz\Control\Campaign\Theater $location
  * @property-read \Buzz\Control\Campaign\Customer $guest
  * @property-read \Buzz\Control\Campaign\Customer $host
  * @property-read \Buzz\Control\Campaign\MeetingSlot $meeting_slot
  */
 class Meeting extends SdkObject
 {
+    use SupportRead;
+
+    public function customerGuestAvailability(Customer $customer): array
+    {
+        return $this->api()->get($this->getEndpoint(sprintf('%s/customer-guest-availability', $customer->id)));
+    }
+
+    public function customerHostAvailability(Customer $guest, Customer $host): array
+    {
+        return $this->api()
+            ->get($this->getEndpoint(sprintf('%s/%s/customer-host-availability', $guest->id, $host->id)));
+    }
+
+    public function reschedule(Customer $customer, MeetingSlot $meetingSlot): EssentialsSdkObject
+    {
+        return Cast::single(
+            (new Meeting()),
+            $this->api()->post(
+                $this->getEndpoint(sprintf('%s/%s/reschedule/%s', $customer->id, $this->id, $meetingSlot->id)),
+                request()->all()
+            )
+        );
+    }
+
+    public function cancel(): bool
+    {
+        return $this->api()->post(
+            $this->getEndpoint(sprintf('%s/cancel', $this->id)),
+            request()->all()
+        );
+    }
 }
--- src/Campaign/MeetingRequest.php
+++ src/Campaign/MeetingRequest.php
@@ -2,17 +2,91 @@
 
 namespace Buzz\Control\Campaign;
 
+use Buzz\Control\Traits\SupportRead;
+use Buzz\EssentialsSdk\Cast;
+use Buzz\EssentialsSdk\SdkObject as EssentialsSdkObject;
+
 /**
- * Class Meeting
+ * Class MeetingRequest
  *
- * @property int $status
+ * @property string $status
+ * @property string $location_id
+ * @property string $location_other
  * @property string $guest_id
  * @property string $host_id
  * @property string $meeting_slot_id
+ * @property-read \Buzz\Control\Campaign\Theater $location
  * @property-read \Buzz\Control\Campaign\Customer $guest
  * @property-read \Buzz\Control\Campaign\Customer $host
  * @property-read \Buzz\Control\Campaign\MeetingSlot $meeting_slot
  */
 class MeetingRequest extends SdkObject
 {
+    use SupportRead;
+
+    public function requestWithCustomer(Customer $host, Customer $guest, MeetingSlot $meetingSlot): EssentialsSdkObject
+    {
+        return Cast::single(
+            (new MeetingRequest()),
+            $this->api()->post(
+                $this->getEndpoint(sprintf('%s/%s/%s/request-with-customer', $host->id, $guest->id, $meetingSlot->id)),
+                request()->all()
+            )
+        );
+    }
+
+    public function requestWithExhibitor(
+        Exhibitor $host,
+        Customer $guest,
+        MeetingSlot $meetingSlot
+    ): EssentialsSdkObject {
+        return Cast::single(
+            (new MeetingRequest()),
+            $this->api()->post(
+                $this->getEndpoint(sprintf('%s/%s/%s/request-with-exhibitor', $host->id, $guest->id, $meetingSlot->id)),
+                request()->all()
+            )
+        );
+    }
+
+    public function reschedule(Customer $customer, MeetingSlot $meetingSlot): EssentialsSdkObject
+    {
+        return Cast::single(
+            (new MeetingRequest()),
+            $this->api()->post(
+                $this->getEndpoint(sprintf('%s/%s/reschedule/%s', $customer->id, $this->id, $meetingSlot->id)),
+                request()->all()
+            )
+        );
+    }
+
+    public function cancel(): bool
+    {
+        return $this->api()->post(
+            $this->getEndpoint(sprintf('%s/cancel', $this->id)),
+            request()->all()
+        );
+    }
+
+    public function accept(): EssentialsSdkObject
+    {
+        return Cast::single(
+            (new MeetingRequest()),
+            $this->api()->post(
+                $this->getEndpoint(sprintf('%s/accept', $this->id)),
+                request()->all()
+            )
+        );
+    }
+
+    public function decline(): EssentialsSdkObject
+    {
+        return Cast::single(
+            (new MeetingRequest()),
+            $this->api()->post(
+                $this->getEndpoint(sprintf('%s/decline', $this->id)),
+                request()->all()
+            )
+        );
+    }
 }
