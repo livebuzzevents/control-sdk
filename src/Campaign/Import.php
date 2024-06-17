6ac79db43d8c9a9ecf118d1b2f1fec7aacb07611
Jacketh <jack.handley@livebuzz.co.uk>
2024-06-17 14:46:59 +0100

    added support read and exhibitor endpoint to import model

--- src/Campaign/Import.php
+++ src/Campaign/Import.php
@@ -2,17 +2,31 @@
 
 namespace Buzz\Control\Campaign;
 
+use Buzz\Control\Traits\SupportRead;
+use Buzz\EssentialsSdk\Cast;
+
 /**
  * Class Import
  *
  * @property string $name
  * @property string $user_id
+ * @property string $exhibitor_id
  * @property string $type
  * @property array $settings
  * @property boolean $configured
  * @property-read int $imported
  * @property-read \Buzz\Control\Gateway\User $user
+ * @property-read \Buzz\Control\Campaign\Exhibitor $exhibitor
  */
 class Import extends SdkObject
 {
+    use SupportRead;
+
+    public function exhibitor(Exhibitor $exhibitor)
+    {
+        return Cast::single(
+            (new Import()),
+            $this->api()->post($this->getEndpoint(sprintf('exhibitor/%s', $exhibitor->id)), request()->all())
+        );
+    }
 }
