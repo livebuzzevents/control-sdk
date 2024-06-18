a48ed816b9297907595369edb8e621764c8aae7f
Jacketh <jack.handley@livebuzz.co.uk>
2024-06-18 14:21:47 +0100

    added get imports endpoint to exhibitor model

--- src/Campaign/Exhibitor.php
+++ src/Campaign/Exhibitor.php
@@ -125,4 +125,18 @@ class Exhibitor extends SdkObject
     {
         return collect($this->api()->get('app/fetch-exhibitor-filters'));
     }
+
+    /**
+     * @return \Buzz\EssentialsSdk\Collection
+     * @throws \Buzz\EssentialsSdk\Exceptions\ErrorException
+     */
+    public function getImports(): Collection
+    {
+        return Cast::many(
+            (new Import()),
+            $this->api()->get(
+                $this->getEndpoint($this->id . '/imports')
+            )
+        );
+    }
 }
