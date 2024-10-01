bd30ddce928930ee866245140a3e452dd018cd6e
Jacketh <jack.handley@livebuzz.co.uk>
2024-10-01 18:39:13 +0100

    added seminar exhibitor model

---
+++ src/Campaign/SeminarExhibitor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Buzz\Control\Campaign;
+
+/**
+ * Class SeminarExhibitor
+ *
+ * @property string $seminar_id
+ * @property string $exhibitor_id
+ * @property-read \Buzz\Control\Campaign\Seminar $seminar
+ */
+class SeminarExhibitor extends SdkObject
+{
+}
