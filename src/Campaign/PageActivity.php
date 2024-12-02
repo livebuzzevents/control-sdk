6c3c60d79f793c526a58607a8ad9358bc18bd9e2
Jacketh <jack.handley@livebuzz.co.uk>
2024-12-02 16:26:35 +0000

    added target fields to page activity model

--- src/Campaign/PageActivity.php
+++ src/Campaign/PageActivity.php
@@ -9,10 +9,12 @@ use Buzz\Control\Traits\SupportWrite;
  * Class PageActivity
  *
  * @property string $customer_id
- * @property string $target_id
  * @property string $exhibitor_id
+ * @property string $target_id
+ * @property string $target_type
  * @property string $page_id
  * @property int $action
+ * @property-read object $target
  * @property-read \Buzz\Control\Campaign\Customer $customer
  * @property-read \Buzz\Control\Campaign\Exhibitor $exhibitor
  * @property-read \Buzz\Control\Campaign\Page $page
