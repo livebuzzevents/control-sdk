b7dab2b3ee72ebe45701a206995cce17459729c1
Lee Aplin <lee.aplin@livebuzz.co.uk>
2024-12-06 16:17:30 +0000

    Add conversation model

--- src/Campaign/ChatConversation.php
+++ src/Campaign/ChatConversation.php
@@ -8,22 +8,15 @@ use Buzz\Control\Traits\SupportWrite;
 /**
  * Class ChatConversation
  *
- * @property string $sender_type
- * @property string $sender_id
- * @property string $recipient_type
- * @property string $recipient_id
- * @property string $message
- * @property bool $read
- * @property string $conversation_id
  */
 class ChatConversation extends SdkObject
 {
     use SupportRead, SupportWrite;
 
-    public function create(string $conversation_id): ChatConversation
+    public function create(string $conversation_id)
     {
         return $this->api()->post(
-            $this->getEndpoint('chat-conversations/create'),
+            $this->getEndpoint('create'),
             [
                 'id' => $conversation_id,]
         );
