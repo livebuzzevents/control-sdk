9bd5ba6f8f91d8b31153de6ab5a540fb18f22a31
Jacketh <jack.handley@livebuzz.co.uk>
2025-01-10 15:23:56 +0000

    added custom type model

---
+++ src/Campaign/CustomType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Buzz\Control\Campaign;
+
+use Buzz\Control\Campaign\Traits\Translatable;
+
+/**
+ * Class CustomStatus
+ *
+ * @property string $identifier
+ * @property string $name
+ * @property string $color
+ * @property string $text_colour
+ * @property string $type
+ * @property int $order
+ */
+class CustomType extends SdkObject
+{
+    use Translatable;
+}
