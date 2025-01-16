079662a730f0f7c22ccfdb9db6ef6e67cf0345f0
Jacketh <jack.handley@livebuzz.co.uk>
2025-01-16 09:18:35 +0000

    added creator field to note model

--- src/Campaign/Note.php
+++ src/Campaign/Note.php
@@ -8,10 +8,12 @@ use Buzz\Control\Campaign\Traits\Morphable;
  * Class Note
  *
  * @property string $user_id
+ * @property string $creator_id
+ * @property string $model_id
+ * @property string $model_type
  * @property string $value
- *
  * @property-read \Buzz\Control\Gateway\User $user
- *
+ * @property-read \Buzz\Control\Campaign\Customer $creator
  */
 class Note extends SdkObject
 {
