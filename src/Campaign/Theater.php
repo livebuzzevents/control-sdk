66bfe7036511ec657962e755016b74aef223576f
Jack Handley <jack.handley@livebuzz.co.uk>
2025-05-16 09:47:41 +0100

    added meeting locations endpoint

--- src/Campaign/Theater.php
+++ src/Campaign/Theater.php
@@ -4,6 +4,7 @@ namespace Buzz\Control\Campaign;
 
 use Buzz\Control\Traits\SupportRead;
 use Buzz\Control\Traits\SupportWrite;
+use Buzz\EssentialsSdk\Cast;
 
 /**
  * Class Theater
@@ -13,10 +14,18 @@ use Buzz\Control\Traits\SupportWrite;
  * @property string $type
  * @property string $color
  * @property string $text_color_hexstring
- * @property-read \Buzz\Control\Campaign\Seminar[] $pages
+ * @property-read \Buzz\Control\Campaign\Seminar[] $seminars
  */
 class Theater extends SdkObject
 {
     use SupportRead,
         SupportWrite;
+
+    public function meetingLocations(Customer $customer)
+    {
+        return Cast::many(
+            (new Theater()),
+            $this->api()->get($this->getEndpoint(sprintf('%s/%s', $customer->id, 'meeting-locations')))
+        );
+    }
 }
